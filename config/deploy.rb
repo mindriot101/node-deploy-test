97f71df14aeff084c7d951d1ba4aa8e642ee4e36
Simon Walker <s.r.walker101@googlemail.com>
2013-11-06 23:19:42 +0000

    Adds capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,37 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "myapp"
+set :scm, "git"
+set :repository, "git@github.com:mindriot101/node-deploy-test.git"
+set :branch, "master"
+set :deploy_to, "/var/www/app"
+set :deploy_via, :remote_cache
+set :copy_strategy, :checkout
+set :keep_releases, 5
+set :use_sudo, false
+set :copy_compression, :bz2
+set :normalize_asset_timestamps, false
+set :document_root, "/var/www/app"
+set :ssh_options, { :forward_agent => true, :port => 2222 }
+set :user, "deploy"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :app, "127.0.0.1"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+namespace :deploy do
+  task :start, :roles => :app do
+    run "sudo restart #{application} || sudo start #{application}"
+  end
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+  task :stop, :roles => :app do
+    run "sudo stop #{application}"
+  end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+  task :restart, :roles => :app do
+    start
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  task :npm_install, :roles => :app do
+    run "cd #{release_path} && npm install"
+  end
+end
+
+after "deploy:update", "deploy:cleanup"
+after "deploy:update_code", "deploy:npm_install"
